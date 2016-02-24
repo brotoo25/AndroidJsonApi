20efb9a6b02483204c8ae164446cb3f2cefe3898
Abraao Lima <abraao@bankfacil.com.br>
2016-02-24 18:40:19 -0300

    - Annotation used to check if POJO structure is right

--- jsonapi-compiler/src/main/java/jsonapi/JsonApiProcessor.java
+++ jsonapi-compiler/src/main/java/jsonapi/JsonApiProcessor.java
@@ -1,11 +1,7 @@
 package jsonapi;
 
 import com.google.auto.service.AutoService;
-import com.squareup.javapoet.JavaFile;
-import com.squareup.javapoet.MethodSpec;
-import com.squareup.javapoet.TypeSpec;
 
-import java.io.IOException;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -19,14 +15,8 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
 
-import static com.squareup.javapoet.JavaFile.builder;
-import static com.squareup.javapoet.MethodSpec.methodBuilder;
-import static com.squareup.javapoet.TypeName.get;
-import static com.squareup.javapoet.TypeSpec.classBuilder;
 import static javax.lang.model.SourceVersion.latestSupported;
-import static javax.lang.model.element.Modifier.PUBLIC;
 import static javax.tools.Diagnostic.Kind.ERROR;
-import static jsonapi.Utils.getPackageName;
 
 /**
  * Created by broto on 22/02/16.
@@ -62,92 +52,32 @@ public class JsonApiProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+
         for (Element annotatedElement : roundEnv.getElementsAnnotatedWith(JsonApiResource.class)) {
-            // Our annotation is defined with @Target(value=TYPE). Therefore, we can assume that
-            // this annotatedElement is a TypeElement.
+            boolean hasIdField = false, hasTypeField = false;
+
             TypeElement annotatedClass = (TypeElement) annotatedElement;
 
+            for (Element element : annotatedClass.getEnclosedElements()) {
+                if (!(element instanceof VariableElement)) {
+                    continue;
+                }
+                VariableElement variableElement = (VariableElement) element;
 
-            String packageName = getPackageName(processingEnv.getElementUtils(), annotatedClass);
+                if (variableElement.getSimpleName().toString().equals("id"))
+                    hasIdField = true;
 
-            try {
-                TypeSpec generatedClass = generateClass(annotatedClass);
-                JavaFile javaFile = builder(packageName, generatedClass).build();
+                if (variableElement.getSimpleName().toString().equals("type"))
+                    hasTypeField = true;
+            }
 
-                javaFile.writeTo(processingEnv.getFiler());
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (!hasIdField || !hasTypeField) {
+                error(annotatedElement, "Object does not hold an Id field or a Type field!");
+                return false;
             }
         }
 
         return true;
-    }
-
-    public TypeSpec generateClass(TypeElement typeElement) {
-        TypeSpec.Builder builder = classBuilder(PREFIX + typeElement.getSimpleName().toString())
-                .addModifiers(PUBLIC);
-
-        for (Element element : typeElement.getEnclosedElements()) {
-            if (!(element instanceof VariableElement)) {
-                continue;
-            }
-            VariableElement variableElement = (VariableElement) element;
-
-            builder.addField(get(variableElement.asType()), variableElement.getSimpleName().toString());
-            builder.addMethod(makeGetMethod(variableElement));
-            builder.addMethod(makeSetMethod(variableElement));
-        }
-
-        builder.addField(String.class, "id");
-        builder.addMethod(makeSetMethod("id"));
-        builder.addMethod(makeGetMethod("id", String.class));
-
-        builder.addField(String.class, "type");
-        builder.addMethod(makeSetMethod("type"));
-        builder.addMethod(makeGetMethod("type", String.class));
-
-        return builder.build();
-    }
-
-    private MethodSpec makeSetMethod(String field) {
-        return methodBuilder("set" + toCamelCase(field))
-                .addJavadoc("@return string suitable for {@param instance}'s toString()")
-                .addModifiers(PUBLIC)
-                .addStatement("this." + field + " = " + field)
-                .returns(void.class)
-                .build();
-    }
-
-    private MethodSpec makeSetMethod(VariableElement variableElement) {
-        return methodBuilder("set" + toCamelCase(variableElement.getSimpleName().toString()))
-                .addJavadoc("@return string suitable for {@param instance}'s toString()")
-                .addModifiers(PUBLIC)
-                .addStatement("this." + variableElement.getSimpleName().toString() + " = " + variableElement.getSimpleName().toString())
-                .returns(void.class)
-                .build();
-    }
-
-    private MethodSpec makeGetMethod(String field, Class type) {
-        return methodBuilder("get" + toCamelCase(field))
-                .addJavadoc("@return string suitable for {@param instance}'s toString()")
-                .addModifiers(PUBLIC)
-                .addStatement("return " + field)
-                .returns(type)
-                .build();
-    }
-
-    private MethodSpec makeGetMethod(VariableElement variableElement) {
-        return methodBuilder("get" + toCamelCase(variableElement.getSimpleName().toString()))
-                .addJavadoc("@return string suitable for {@param instance}'s toString()")
-                .addModifiers(PUBLIC)
-                .addStatement("return " + variableElement.getSimpleName().toString())
-                .returns(get(variableElement.asType()))
-                .build();
-    }
-
-    private String toCamelCase(String s) {
-        return s.substring(0, 1).toUpperCase() +
-                s.substring(1).toLowerCase();
     }
 
     private void error(Element element, String message, Object... args) {
