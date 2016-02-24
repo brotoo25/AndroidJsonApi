c564c754c9f5dd1821072abc21e416f846ea8d47
Abraao Lima <abraao@bankfacil.com.br>
2016-02-24 17:46:31 -0300

    - Processor generating classes with basic JsonApi variables

--- jsonapi-compiler/src/main/java/jsonapi/JsonApiProcessor.java
+++ jsonapi-compiler/src/main/java/jsonapi/JsonApiProcessor.java
@@ -1,8 +1,11 @@
 package jsonapi;
 
 import com.google.auto.service.AutoService;
+import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
 
+import java.io.IOException;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -14,24 +17,32 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
 
+import static com.squareup.javapoet.JavaFile.builder;
 import static com.squareup.javapoet.MethodSpec.methodBuilder;
+import static com.squareup.javapoet.TypeName.get;
+import static com.squareup.javapoet.TypeSpec.classBuilder;
 import static javax.lang.model.SourceVersion.latestSupported;
 import static javax.lang.model.element.Modifier.PUBLIC;
 import static javax.tools.Diagnostic.Kind.ERROR;
+import static jsonapi.Utils.getPackageName;
 
 /**
  * Created by broto on 22/02/16.
  */
 
 @AutoService(Processor.class)
-public class JsonApiProcessor extends AbstractProcessor{
+public class JsonApiProcessor extends AbstractProcessor {
 
+    private static final String PREFIX = "JsonApi$";
     private Messager messager;
+    private ProcessingEnvironment processingEnv;
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
+        this.processingEnv = processingEnv;
         messager = processingEnv.getMessager();
     }
 
@@ -55,30 +66,86 @@ public class JsonApiProcessor extends AbstractProcessor{
             // Our annotation is defined with @Target(value=TYPE). Therefore, we can assume that
             // this annotatedElement is a TypeElement.
             TypeElement annotatedClass = (TypeElement) annotatedElement;
+
+
+            String packageName = getPackageName(processingEnv.getElementUtils(), annotatedClass);
+
+            try {
+                TypeSpec generatedClass = generateClass(annotatedClass);
+                JavaFile javaFile = builder(packageName, generatedClass).build();
+
+                javaFile.writeTo(processingEnv.getFiler());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         return true;
     }
 
-    private static MethodSpec makeSetMethod(String field) {
+    public TypeSpec generateClass(TypeElement typeElement) {
+        TypeSpec.Builder builder = classBuilder(PREFIX + typeElement.getSimpleName().toString())
+                .addModifiers(PUBLIC);
+
+        for (Element element : typeElement.getEnclosedElements()) {
+            if (!(element instanceof VariableElement)) {
+                continue;
+            }
+            VariableElement variableElement = (VariableElement) element;
+
+            builder.addField(get(variableElement.asType()), variableElement.getSimpleName().toString());
+            builder.addMethod(makeGetMethod(variableElement));
+            builder.addMethod(makeSetMethod(variableElement));
+        }
+
+        builder.addField(String.class, "id");
+        builder.addMethod(makeSetMethod("id"));
+        builder.addMethod(makeGetMethod("id", String.class));
+
+        builder.addField(String.class, "type");
+        builder.addMethod(makeSetMethod("type"));
+        builder.addMethod(makeGetMethod("type", String.class));
+
+        return builder.build();
+    }
+
+    private MethodSpec makeSetMethod(String field) {
         return methodBuilder("set" + toCamelCase(field))
                 .addJavadoc("@return string suitable for {@param instance}'s toString()")
                 .addModifiers(PUBLIC)
-                .addStatement("this." + field + "= " + field)
+                .addStatement("this." + field + " = " + field)
                 .returns(void.class)
                 .build();
     }
 
-    private static MethodSpec makeGetMethod(String field) {
+    private MethodSpec makeSetMethod(VariableElement variableElement) {
+        return methodBuilder("set" + toCamelCase(variableElement.getSimpleName().toString()))
+                .addJavadoc("@return string suitable for {@param instance}'s toString()")
+                .addModifiers(PUBLIC)
+                .addStatement("this." + variableElement.getSimpleName().toString() + " = " + variableElement.getSimpleName().toString())
+                .returns(void.class)
+                .build();
+    }
+
+    private MethodSpec makeGetMethod(String field, Class type) {
         return methodBuilder("get" + toCamelCase(field))
                 .addJavadoc("@return string suitable for {@param instance}'s toString()")
                 .addModifiers(PUBLIC)
                 .addStatement("return " + field)
-                .returns(String.class)
+                .returns(type)
                 .build();
     }
 
-    private static String toCamelCase(String s) {
+    private MethodSpec makeGetMethod(VariableElement variableElement) {
+        return methodBuilder("get" + toCamelCase(variableElement.getSimpleName().toString()))
+                .addJavadoc("@return string suitable for {@param instance}'s toString()")
+                .addModifiers(PUBLIC)
+                .addStatement("return " + variableElement.getSimpleName().toString())
+                .returns(get(variableElement.asType()))
+                .build();
+    }
+
+    private String toCamelCase(String s) {
         return s.substring(0, 1).toUpperCase() +
                 s.substring(1).toLowerCase();
     }
